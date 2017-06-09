38bdd1105a0050c56c5f610bb94f7f60a7972f37
Mina <mina.shaigan@gmail.com>
2017-06-10 02:43:35 +0430

    Add files via upload

---
+++ chp 3/3.23/QuoteClient.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quoteclient;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author x450c1
+ */
+public class QuoteClient {
+
+    public static void main(String[] args)  {
+		try {
+			// this could be changed to an IP name or address other than the localhost
+			Socket sock = new Socket("127.0.0.1",6017);
+			InputStream in = sock.getInputStream();
+			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+			String line;
+			while( (line = bin.readLine()) != null)
+				System.out.println(line);
+				
+			sock.close();
+		}
+		catch (IOException ioe) {
+				System.err.println(ioe);
+		}
+	}
+    
+}
